cbdd7da7ace681cb1030d5f52a397e9a3a983d69
Mr. Sachin <16249486+sachinkrb@users.noreply.github.com>
2019-03-02 17:20:43 -0600

    Update listDictTupSetLoop.py

--- listDictTupSetLoop.py
+++ listDictTupSetLoop.py
@@ -103,4 +103,79 @@ AttributeError: module 'sys' has no attribute 'ps2'
 >>> import builtins
 >>> dir(builtins)
 ['ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException', 'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning', 'ChildProcessError', 'ConnectionAbortedError', 'ConnectionError', 'ConnectionRefusedError', 'ConnectionResetError', 'DeprecationWarning', 'EOFError', 'Ellipsis', 'EnvironmentError', 'Exception', 'False', 'FileExistsError', 'FileNotFoundError', 'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning', 'IndentationError', 'IndexError', 'InterruptedError', 'IsADirectoryError', 'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError', 'ModuleNotFoundError', 'NameError', 'None', 'NotADirectoryError', 'NotImplemented', 'NotImplementedError', 'OSError', 'OverflowError', 'PendingDeprecationWarning', 'PermissionError', 'ProcessLookupError', 'RecursionError', 'ReferenceError', 'ResourceWarning', 'RuntimeError', 'RuntimeWarning', 'StopAsyncIteration', 'StopIteration', 'SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError', 'TimeoutError', 'True', 'TypeError', 'UnboundLocalError', 'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'WindowsError', 'ZeroDivisionError', '_', '__build_class__', '__debug__', '__doc__', '__import__', '__loader__', '__name__', '__package__', '__spec__', 'abs', 'all', 'any', 'ascii', 'bin', 'bool', 'breakpoint', 'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'exit', 'filter', 'float', 'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'license', 'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip']
+>>> software = {}
+>>> software['pinger'] = 100
+>>> software
+{'pinger': 100}
+>>> software[100]
+Traceback (most recent call last):
+  File "<pyshell#62>", line 1, in <module>
+    software[100]
+KeyError: 100
+>>> software['pinger'}
+SyntaxError: invalid syntax
+>>> software['pinger']
+100
+>>> for key in software.keys()
+SyntaxError: invalid syntax
+>>> for key in software.keys():
+	print(key)
+
+	
+pinger
+>>> for value in software.values():
+	print(value)
+
+	
+100
+>>> for key_value in software.items():
+	print(key_value)
+
+	
+('pinger', 100)
+>>> my_file = open('testfile.txt', 'wt')
+>>> my_file.write('This is a test file\n')
+20
+>>> my_file.write('How to read,write,delete and open files
+		  
+SyntaxError: EOL while scanning string literal
+>>> my_file.write('How to read, write,delete and open files')
+		  
+40
+>>> my_file.close()
+		  
+>>> my_file2.read()
+		  
+Traceback (most recent call last):
+  File "<pyshell#80>", line 1, in <module>
+    my_file2.read()
+NameError: name 'my_file2' is not defined
+>>> my_file
+		  
+<_io.TextIOWrapper name='testfile.txt' mode='wt' encoding='cp1252'>
+>>> my_file =open('testfile.txt','rt')
+		  
+>>> contents = my_file.read()
+		  
+>>> print(contents)
+		  
+This is a test file
+How to read, write,delete and open files
+>>> my_file.close()
+		  
+>>> my_file = open('testfile','rt')
+		  
+Traceback (most recent call last):
+  File "<pyshell#86>", line 1, in <module>
+    my_file = open('testfile','rt')
+FileNotFoundError: [Errno 2] No such file or directory: 'testfile'
+>>> my_file= open('testfile.txt','rt')
+		  
+>>> for line in my_file:
+		  print('line is: ', line)
+
+		  
+line is:  This is a test file
+
+line is:  How to read, write,delete and open files
 >>> 
